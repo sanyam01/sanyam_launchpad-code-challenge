6bd46c5f1d5082171a28c0b6e4f1ce6e0f67b01d
sanyam kumar <sanyam1@ucalgary.ca>
2022-02-07 11:49:24 -0700

    Added some styling to the universities

--- src/components/Pages/Universities/UniversityLayout.js
+++ src/components/Pages/Universities/UniversityLayout.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
+import classes from "./UniversityLayout.module.css";
+import Card from "../Layout/Card";
 
 const UniversityLayout = () => {
   const data = useSelector((state) => state.universities.countryData);
@@ -8,11 +10,15 @@ const UniversityLayout = () => {
   return (
     <Fragment>
       <h3>This is the university layout</h3>
-      {data && <ul>
-        {data.map((post) => (
-          <p> {JSON.stringify(post)}</p>
-        ))}
-      </ul>}
+      {data && (
+        <div>
+          {data.map((post) => (
+            <Card>
+              <p className={classes.post}> {JSON.stringify(post)}</p>
+            </Card>
+          ))}
+        </div>
+      )}
     </Fragment>
   );
 };
